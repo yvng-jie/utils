00a8cb4b59ac02a7deb7fa728c7e421ade3d3183
jie.yang <24789671+yangfree@users.noreply.github.com>
2021-10-25 13:48:20 +0800

    add utils and style.css

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,4 @@
-/**====================================================================================
- * 1.  时间格式化
- * 2.  对象深度克隆
- * 3.  判断JS执行环境
- * 4.  获取通用查询字符串参数
- * 5.  获取随机数字+字母组成的n位验证码
- * 6.  获取字符串中出现次数最多的字母和出现的次数
- * 7.  数组套对象根据id去重，然后合并其他属性
- * 8.  数组降维
- * 9.  函数节流
- * 10. 数据类型检测
- *=====================================================================================/ 
-
+// ======================================= 时间格式化函数 ==============================================
 /**
  * 作用: 格式化时间 
  * @param {String: date}
@@ -42,6 +30,7 @@ function formatDate(date) {
         formatDate.hour + formatDate.minute + formatDate.senc;
 }
 
+// ======================================= 对象操作 ==============================================
 /**
  * 作用: 对象深度克隆
  * @param: [Object source]
@@ -59,6 +48,7 @@ function objDeepCopy(source) {
     return sourceCopy;
 };
 
+// ======================================= 环境/设备/浏览器判断 ==============================================
 /**
  * 作用: 判断js执行环境
  * @param: window
@@ -73,6 +63,104 @@ function isJavaScript() {
     return "Browser";
 }
 
+/**
+ * 作用: 判断是移动还是PC设备
+ * @param: { Null } 
+ * @return { String } 
+ */
+function isMobile() {
+  if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) {
+  return 'mobile';
+  }
+  return 'desktop';
+}
+
+/**
+ * 作用: 判断是否是苹果还是安卓移动设备
+ * @param: { Null } 
+ * @return { Boolean } 
+ */
+function isAppleMobileDevice() {
+  let reg = /iphone|ipod|ipad|Macintosh/i;
+  return reg.test(navigator.userAgent.toLowerCase());
+}
+
+/**
+ * 作用: 判断是否是安卓移动设备
+ * @param: { Null } 
+ * @return { Boolean } 
+ */
+function isAndroidMobileDevice() {
+    return /android/i.test(navigator.userAgent.toLowerCase());
+}
+
+/**
+ * 作用: 判断是Windows还是Mac系统
+ * @param: { Null } 
+ * @return { Boolean } 
+ */
+function isAndroidMobileDevice() {
+    const agent = navigator.userAgent.toLowerCase();
+    const isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+   const isWindows = agent.indexOf("win64") >= 0 || agent.indexOf("wow64") >= 0 || agent.indexOf("win32") >= 0 || agent.indexOf("wow32") >= 0;
+    if (isWindows) {
+        return "windows";
+    }
+    if(isMac){
+        return "mac";
+    }
+}
+
+/**
+ * 作用: 判断是否是微信/QQ内置浏览器
+ * @param: { Null } 
+ * @return { Boolean } 
+ */
+function isAndroidMobileDevice() {
+    const ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        return "weixin";
+    } else if (ua.match(/QQ/i) == "qq") {
+        return "QQ";
+    }
+    return false;
+}
+
+/**
+ * 作用: 浏览器型号和版本
+ * @param: { Null } 
+ * @return { Boolean } 
+ */
+function isAndroidMobileDevice() {
+    let t = navigator.userAgent.toLowerCase();
+    return 0 <= t.indexOf("msie") ? { //ie < 11
+        type: "IE",
+        version: Number(t.match(/msie ([\d]+)/)[1])
+    } : !!t.match(/trident\/.+?rv:(([\d.]+))/) ? { // ie 11
+        type: "IE",
+        version: 11
+    } : 0 <= t.indexOf("edge") ? {
+        type: "Edge",
+        version: Number(t.match(/edge\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("firefox") ? {
+        type: "Firefox",
+        version: Number(t.match(/firefox\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("chrome") ? {
+        type: "Chrome",
+        version: Number(t.match(/chrome\/([\d]+)/)[1])
+    } : 0 <= t.indexOf("opera") ? {
+        type: "Opera",
+        version: Number(t.match(/opera.([\d]+)/)[1])
+    } : 0 <= t.indexOf("Safari") ? {
+        type: "Safari",
+        version: Number(t.match(/version\/([\d]+)/)[1])
+    } : {
+        type: t,
+        version: -1
+    }
+}
+
+// ======================================= URL操作 ==============================================
 /**
  * 作用: 获取通用查询字符串参数
  *  @param: null
@@ -103,6 +191,68 @@ function getQueryStringArgs() {
 }
 
 /**
+ * 作用: 检测URL是否有效
+ *  @param: { String }
+ *  @return { Boolean }
+ * 
+ */
+function getUrlState(URL) {
+  let xmlhttp = new ActiveXObject("microsoft.xmlhttp");
+  xmlhttp.Open("GET", URL, false);
+  try {
+    xmlhttp.Send();
+  } catch (e) {
+  } finally {
+    let result = xmlhttp.responseText;
+    if (result) {
+      if (xmlhttp.Status == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+}
+
+/**
+ * 作用: 键值对拼接成URL参数
+ *  @param: { Object }
+ *  @return { Boolean }
+ * 
+ */
+function params2Url(obj) {
+     let params = []
+     for (let key in obj) {
+       params.push(`${key}=${obj[key]}`);
+     }
+     return encodeURIComponent(params.join('&'))
+}
+
+/**
+ * 作用: 删除URL中指定参数
+ *  @param: { String }
+ *  @return { String }
+ * 
+ */
+function funcUrlDel(name) {
+  const baseUrl = location.origin + location.pathname + "?";
+  const query = location.search.substr(1);
+  if (query.indexOf(name) > -1) {
+    const obj = {};
+    const arr = query.split("&");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].split("=");
+      obj[arr[i][0]] = arr[i][1];
+    }
+    delete obj[name];
+    return baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g,"").replace(/\:/g,"=").replace(/\,/g,"&");
+  }
+}
+
+// ======================================= 字符串操作 ==============================================
+/**
  * 作用: 获取随机数字+字母组成的n位验证码
  * @param {n} Number    参数是位数
  * @return {tem} String  返回随机字符串
@@ -128,7 +278,6 @@ function getRandomStr(len) {
     for (; str.length < len; str += Math.random().toString(36).substr(2));
     return str.substr(0, len);
 }
-
 
 /**
  * 作用: 获取字符串中出现次数最多的字母和出现的次数
@@ -159,6 +308,31 @@ function getStr(str) {
     return { maxChar, maxNum }
 }
 
+/**
+ * 作用: 生成随机字符串
+ * @param: { Number } 随机生成的字符串长度 
+ * @return { String } 字符串
+ */
+function randomString(len) {
+    let chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz123456789';
+    let strLen = chars.length;
+    let randomStr = '';
+    for (let i = 0; i < len; i++) {
+        randomStr += chars.charAt(Math.floor(Math.random() * strLen));
+    }
+    return randomStr;
+}
+
+/**
+ * 作用: 字符串首字母大写
+ * @param: { String }  
+ * @return { String } 
+ */
+function fistLetterUpper(len) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// ======================================= 数组操作 ==============================================
 /**
  * 作用: 数组套对象根据id去重，然后合并其他属性
  *
@@ -199,11 +373,34 @@ function mergeArr(arr) {
 function flatten(arr) {
     return [].concat(
         ...arr.map(x => {
-            Aaary.isArray(x) ? flatten(x) : x;
+            Array.isArray(x) ? flatten(x) : x;
         })
     )
 }
 
+/**
+ * 作用: 数组乱序
+ * @param: { Array } 
+ * @return { Array } 乱序后的数组
+ */
+function arrScrambling(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      const randomIndex = Math.round(Math.random() * (arr.length - 1 - i)) + i;
+      [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+    }
+    return arr;
+}
+
+/**
+ * 作用: 数组中获取随机数
+ * @param: { Array } 
+ * @return { Number } item
+ */
+function sample(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// ======================================= 节流防抖 ==============================================
 /**
  * 作用： 函数节流
  * @param: { fn } 一个函数
@@ -231,10 +428,11 @@ function throttle(fn, delay = 300, I = null) {
     }
 }
 
+// ======================================= 数据类型检测 ==============================================
 /**
  * 作用: 数据类型检测
  * @param: { data } 任意数据
- * @resturn data type 
+ * @return data type 
  */
 function typeCheck(data) {
     const toString = Object.prototype.toString;
@@ -246,4 +444,376 @@ function typeCheck(data) {
                 .toLowerCase()
     )
     return dataType;
+}
+
+// ======================================= 数字操作 ==============================================
+/**
+ * 作用: 生成指定范围内的随机数
+ * @param: { Number } 
+ * @return 随机数
+ */
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * 作用: 数字千分位分隔
+ * @param: { Number } 
+ * @return { String } 分割好的字符串
+ */
+function format(n) {
+    let num = n.toString();
+    let len = num.length;
+    if(len <= 3) {
+        return num;
+    } else {
+        let temp = ''
+        let remainder = len % 3;
+        if(remainder > 0 ) {
+            // 不是3的整倍数
+            return num.slice(0, remainder) + ',' + num.slice(remainder, len).match(/\d{3}/g).join(',') + temp;
+        } else {
+            // 是3的整倍数
+             return num.slice(0, len).match(/\d{3}/g).join(',') + temp; 
+        }
+    }
+}
+
+// ======================================= 工具类 ==============================================
+/**
+ * 作用: 手机号中间四位变成*
+ * @param: { Number | String }  
+ * @return { String } 
+ */
+function telFormat(tel) {
+    typeof tel !== 'string' ? String(tel) : null; 
+    return tel.substr(0,3) + "****" + tel.substr(7);
+}
+
+/**
+ * 作用: 驼峰命名转换成短横线命名
+ * @param: { String }  
+ * @return { String } 
+ */
+function getKebabCase(str) {
+    return str.replace(/[A-Z]/g, (item) => '-' + item.toLowerCase())
+}
+
+/**
+ * 作用: 短横线命名转换成驼峰命名
+ * @param: { String }  
+ * @return { String } 
+ */
+function getCamelCase(str) {
+    return str.replace( /-([a-z])/g, (i, item) => item.toUpperCase())
+}
+
+/**
+ * 作用: 全角转换为半角
+ * @param: { String }  
+ * @return { String } 
+ */
+function toCDB(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    code = str.charCodeAt(i);
+    if (code >= 65281 && code <= 65374) {
+      result += String.fromCharCode(str.charCodeAt(i) - 65248);
+    } else if (code == 12288) {
+      result += String.fromCharCode(str.charCodeAt(i) - 12288 + 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
+
+/**
+ * 作用: 半角转换为全角
+ * @param: { String }  
+ * @return { String } 
+ */
+function toDBC(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    code = str.charCodeAt(i);
+    if (code >= 33 && code <= 126) {
+      result += String.fromCharCode(str.charCodeAt(i) + 65248);
+    } else if (code == 32) {
+      result += String.fromCharCode(str.charCodeAt(i) + 12288 - 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
+
+/**
+ * 作用: 数字转化为大写金额
+ * @param: { Number }  
+ * @return { String } 
+ */
+function digitUppercase(n) {
+    const fraction = ['角', '分'];
+    const digit = [
+        '零', '壹', '贰', '叁', '肆',
+        '伍', '陆', '柒', '捌', '玖'
+    ];
+    const unit = [
+        ['元', '万', '亿'],
+        ['', '拾', '佰', '仟']
+    ];
+    n = Math.abs(n);
+    let s = '';
+    for (let i = 0; i < fraction.length; i++) {
+        s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+    }
+    s = s || '整';
+    n = Math.floor(n);
+    for (let i = 0; i < unit[0].length && n > 0; i++) {
+        let p = '';
+        for (let j = 0; j < unit[1].length && n > 0; j++) {
+            p = digit[n % 10] + unit[1][j] + p;
+            n = Math.floor(n / 10);
+        }
+        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+    }
+    return s.replace(/(零.)*零元/, '元')
+        .replace(/(零.)+/g, '零')
+        .replace(/^整$/, '零元整');
+}
+
+/**
+ * 作用: 校验身份证号码
+ * @param: { Number } 
+ * @return { Boolean } 
+ */
+function checkCardNo(value) {
+    let reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    return reg.test(value);
+}
+
+/**
+ * 作用: 校验是否包含中文
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function haveCNChars(value) {
+    return /[\u4e00-\u9fa5]/.test(value);
+}
+
+/**
+ * 作用: 校验是否为中国大陆的邮政编码
+ * @param: { Number } 
+ * @return { Boolean } 
+ */
+function isPostCode(value) {
+    return /^[1-9][0-9]{5}$/.test(value.toString());
+}
+
+/**
+ * 作用: 校验是否为IPv6地址
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function isIPv6(str) {
+     return Boolean(str.match(/:/g)
+        ?str.match(/:/g).length<=7:false && /::/.test(str)?/^([\da-f]{1,4}(:|::)){1,6}[\da-f]{1,4}$/i.test(str)
+        :/^([\da-f]{1,4}:){7}[\da-f]{1,4}$/i.test(str));
+}
+
+/**
+ * 作用: 校验是否为邮箱地址
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function isEmail(value) {
+     return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value);
+}
+
+/**
+ * 作用: 校验是否为中国大陆手机号
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function isTel(value) {
+     return /^1[3,4,5,6,7,8,9][0-9]{9}$/.test(value.toString());
+}
+
+/**
+ * 作用: 校验是否包含emoji表情
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function isEmojiCharacter(value) {
+   value = String(value);
+    for (let i = 0; i < value.length; i++) {
+        const hs = value.charCodeAt(i);
+        if (0xd800 <= hs && hs <= 0xdbff) {
+            if (value.length > 1) {
+                const ls = value.charCodeAt(i + 1);
+                const uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+                if (0x1d000 <= uc && uc <= 0x1f77f) {
+                    return true;
+                }
+            }
+        } else if (value.length > 1) {
+            const ls = value.charCodeAt(i + 1);
+            if (ls == 0x20e3) {
+                return true;
+            }
+        } else {
+            if (0x2100 <= hs && hs <= 0x27ff) {
+                return true;
+            } else if (0x2B05 <= hs && hs <= 0x2b07) {
+                return true;
+            } else if (0x2934 <= hs && hs <= 0x2935) {
+                return true;
+            } else if (0x3297 <= hs && hs <= 0x3299) {
+                return true;
+            } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030
+                    || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b
+                    || hs == 0x2b50) {
+                return true;
+            }
+        }
+    }
+   return false;
+}
+
+/**
+ * 作用: 校验是否为中国大陆手机号
+ * @param: { String } 
+ * @return { Boolean } 
+ */
+function isTel(value) {
+     return /^1[3,4,5,6,7,8,9][0-9]{9}$/.test(value.toString());
+}
+
+/**
+ * 作用: 滚动到页面顶部
+ * @param: { Null } 
+ * @return { Null } 
+ */
+function scrollToTop() {
+  const height = document.documentElement.scrollTop || document.body.scrollTop;
+  if (height > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, height - height / 8);
+  }
+}
+
+/**
+ * 作用: 滚动到页面底部
+ * @param: { Null } 
+ * @return { Null } 
+ */
+function scrollToBottom() {
+    window.scrollTo(0, document.documentElement.clientHeight);  
+}
+
+/**
+ * 作用: 滚动到指定元素区域
+ * @param: { Element } 
+ * @return { Null } 
+ */
+function smoothScroll(element) {
+    document.querySelector(element).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+/**
+ * 作用: 获取可视窗口高度
+ * @param: { Null } 
+ * @return { String } 
+ */
+function getClientHeight() {
+    let clientHeight = 0;
+    if (document.body.clientHeight && document.documentElement.clientHeight) {
+        clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+    }
+    else {
+        clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+    }
+    return clientHeight;
+}
+
+/**
+ * 作用: 获取可视窗口宽度
+ * @param: { Null } 
+ * @return { String } 
+ */
+function smoothScroll(element) {
+    return (document.compatMode == "BackCompat" ? document.body : document.documentElement).clientWidth;
+}
+
+/**
+ * 作用: 打开浏览器全屏
+ * @param: { Null } 
+ * @return { Null } 
+ */
+function smoothScroll(element) {
+    let element = document.body;
+    if (element.requestFullscreen) {
+      element.requestFullscreen()
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen()
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen()
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullScreen()
+    }
+}
+
+/**
+ * 作用: 退出浏览器全屏
+ * @param: { Null } 
+ * @return { Null } 
+ */
+function smoothScroll(element) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen()
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+}
+
+// ======================================= 本地存储 ==============================================
+/**
+ * 作用: 存储loalStorage
+ * @param: { String }
+ * @param: { String }  
+ * @return { Null } 
+ */
+function loalStorageSet(key , value) {
+    if(!key) return;
+    if(typeof value !== 'string') {
+        value = JSON.stringify(value);
+    }
+    window.localStorage.setItem(key, value);
+}
+
+/**
+ * 作用: 获取localStorage
+ * @param: { String } 
+ * @return { String } 
+ */
+function loalStorageGet(key) {
+    if(!key) return;
+    return window.localStorage.getItem(key);
+}
+
+/**
+ * 作用: 删除localStorage
+ * @param: { String } 
+ * @return { Null } 
+ */
+function loalStorageRemove(key) {
+    if(!key) return;
+    window.localStorage.removeItem(key);
 }
